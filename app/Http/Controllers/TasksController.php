5082731690b1ac7e771c54620429562fee8e7037
aa.yy <damyd9108@gmail.com>
2022-03-01 08:07:49 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -51,6 +51,7 @@ class TasksController extends Controller
     {
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -107,6 +108,7 @@ class TasksController extends Controller
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,6 +10,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -17,9 +18,9 @@
                 @foreach ($tasks as $task)
                 <tr>
                     
-                    {{-- メッセージ詳細ページへのリンク --}}
+                    {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
